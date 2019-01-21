2e00a038ac20bf353f651fdec1553e22b929967d
Andre Vallestero <andrevallestero@gmail.com>
2019-01-21 16:53:13 -0500

    Completed hello-triangle

--- legacy-opengl/hello-triangle/src/main.cpp
+++ legacy-opengl/hello-triangle/src/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(void) {
-    std::cout << "Hello, World!";
+    glfwInit();
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Title", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	
+	while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+		
+        glBegin(GL_TRIANGLES);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+		
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+	}
+	
+	glfwDestroyWindow(window);
+	glfwTerminate();
     return 0;
 }
