eac94ab8d5525b661c17a21dc16f7843b9787a57
Andre Vallestero <andrevallestero@gmail.com>
2019-01-22 11:26:59 -0500

    Re-added triangle and title for legacy helo-triangle

--- legacy-opengl/hello-triangle/src/main.cpp
+++ legacy-opengl/hello-triangle/src/main.cpp
@@ -2,12 +2,18 @@
 
 int main(void) {
     glfwInit();
-	GLFWwindow* window = glfwCreateWindow(640, 480, "pong", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "hello-triangle", NULL, NULL);
 	glfwMakeContextCurrent(window);
-	
+
 	while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
-		
+
+        glBegin(GL_TRIANGLES);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
 	}
